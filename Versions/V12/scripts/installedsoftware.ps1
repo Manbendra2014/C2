b4da4be211ef9ad9889d6bfb10adde9ed52aca32
Manbendra Satpathy <92919625+Manbendra2014@users.noreply.github.com>
2024-11-29 22:00:08 +0530

    Delete Versions/V12 directory

--- Versions/V12/scripts/installedsoftware.ps1
+++
@@ -1,6 +0,0 @@
-# List Installed Software
-
-$installedSoftware = Get-WmiObject -Class Win32_Product | Select-Object Name, Version
-
-Write-Output "Installed Software:"
-$installedSoftware | ForEach-Object { Write-Output "$($_.Name) - $($_.Version)" }
--- Versions/V12/scripts/soundcheck.ps1
+++
@@ -1,4 +0,0 @@
-# Run a beep sound-check
-for ($i = 500; $i -lt 2000; $i += 100) {
-    [console]::beep($i, 200)
-}
